973f4c34736fdc15bcc8b8b56f2edc946653bba2
Jeff Kissel <jeffrey.kissel@ligo.org>
2020-06-10 12:56:02 -0700

    Commit quick script to spit out python and python package versions installed on users local machine.

---
+++ utils/versioncheck.py
@@ -0,0 +1,30 @@
+import matplotlib
+import scipy
+import sklearn
+import numpy
+import gwpy
+import nds2
+#import guardian
+import sys
+import IIRrational.v2
+import h5py
+import emcee
+import corner
+
+print('    ')
+print('This is Python version:')
+print(sys.version)
+print('')
+print('If you import the following packages, you will get the versions listed below:')
+print('    matplotlib.__version__ = '+matplotlib.__version__)
+print('    numpy.__version__ = '+numpy.__version__)
+print('    scipy.__version__ = '+scipy.__version__)
+print('    sklearn.__version__ = '+sklearn.__version__)
+print('    gwpy.__version__ = '+gwpy.__version__)
+print('    nds2.__version__ = '+nds2.__version__)
+#print('    guardian.__version__ = '+guardian.__version__)
+print('    IIRrational.__version__ = '+IIRrational.__version__)
+print('    h5py.__version__ = '+h5py.__version__)
+print('    emcee.__version__ = '+emcee.__version__)
+print('    corner.__version__ = '+corner.__version__)
+print('    ')
